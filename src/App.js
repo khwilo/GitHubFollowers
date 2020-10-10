12e17eb5711d4b0f34e5c1c12edb92fa23e3ecb3
Khwilo Kabaka <khwilowatai@gmail.com>
2020-10-10 13:14:23 +0300

    Move App.js file into the src folder

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>GitHub Followers</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
