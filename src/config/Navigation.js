70342a4ce0131749fdf4eb6ace6249687561ef01
Khwilo Kabaka <khwilowatai@gmail.com>
2020-10-19 14:38:16 +0300

    Set up the initial bottom tabs navigator

--- src/config/Navigation.js
+++ src/config/Navigation.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { FollowersContextProvider } from '../contexts/FollowersContext';
 
+import FavoritesScreen from '../screens/Favorites';
 import FollowerProfileScreen from '../screens/FollowerProfile';
 import FollowersDisplayScreen from '../screens/FollowersDisplay';
 import HomeScreen from '../screens/Home';
 
 const MainStack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeTabs = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Search" component={HomeScreen} />
+    <Tab.Screen name="Favorites" component={FavoritesScreen} />
+  </Tab.Navigator>
+);
 
 const MainStackScreen = () => (
   <MainStack.Navigator headerMode="none">
-    <MainStack.Screen name="Home" component={HomeScreen} />
+    <MainStack.Screen name="Home" component={HomeTabs} />
     <MainStack.Screen
       name="Followers display"
       component={FollowersDisplayScreen}
---
+++ src/screens/Favorites.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Favorites = () => (
+  <View>
+    <Text>Favorites Screen</Text>
+  </View>
+);
+
+export default Favorites;
