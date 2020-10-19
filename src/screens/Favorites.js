2111c5a412a9da51950d7772538dbe8a4af66843
Khwilo Kabaka <khwilowatai@gmail.com>
2020-10-19 15:07:39 +0300

    Replace the View with SafeAreaView

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Favorites = () => (
-  <View>
+  <SafeAreaView>
     <Text>Favorites Screen</Text>
-  </View>
+  </SafeAreaView>
 );
 
 export default Favorites;
