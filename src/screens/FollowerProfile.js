77891f573a84f3d34d514e1f633edf6eb259f72d
Khwilo Kabaka <khwilowatai@gmail.com>
2020-10-19 13:43:31 +0300

    Add an explicit go back button

--- src/screens/FollowerProfile.js
+++ src/screens/FollowerProfile.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Button } from 'react-native';
 
-const FollowerProfile = () => (
+const FollowerProfile = ({ navigation }) => (
   <View>
     <Text>Follower Profile</Text>
+    <Button title="Go back" onPress={() => navigation.pop()} />
   </View>
 );
 
