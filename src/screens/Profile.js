f6d31de6331ba57b70d1df64b089a4f287e4553b
Khwilo Kabaka <khwilowatai@gmail.com>
2020-10-22 12:33:11 +0300

    Change the button color to green

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -2,10 +2,16 @@ import React from 'react';
 import { Button, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import colors from '../constants/colors';
+
 const Profile = ({ navigation }) => (
   <SafeAreaView>
     <Text>Follower Profile</Text>
-    <Button title="Go back" onPress={() => navigation.goBack()} />
+    <Button
+      title="Go back"
+      color={colors.green}
+      onPress={() => navigation.goBack()}
+    />
   </SafeAreaView>
 );
 
