34e7e48523022c51c5d06526ab4495751dea0b5a
Khwilo Kabaka <khwilowatai@gmail.com>
2020-10-22 07:53:03 +0300

    Rename screens

--- src/screens/FollowersList.js
+++ src/screens/FollowersList.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Button, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const FollowersDisplay = ({ navigation }) => (
+const FollowersList = ({ navigation }) => (
   <SafeAreaView>
     <Text>Followers screen!</Text>
     <Button
       title="View profile"
-      onPress={() => navigation.navigate('Follower profile')}
+      onPress={() => navigation.navigate('Profile')}
     />
   </SafeAreaView>
 );
 
-export default FollowersDisplay;
+export default FollowersList;
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Button, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const FollowerProfile = ({ navigation }) => (
+const Profile = ({ navigation }) => (
   <SafeAreaView>
     <Text>Follower Profile</Text>
-    <Button title="Go back" onPress={() => navigation.pop()} />
+    <Button title="Go back" onPress={() => navigation.goBack()} />
   </SafeAreaView>
 );
 
-export default FollowerProfile;
+export default Profile;
