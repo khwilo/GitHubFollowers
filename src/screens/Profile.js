322353c66b07a72066a081d903b0f3a7cbee9a6b
Khwilo Kabaka <khwilowatai@gmail.com>
2020-10-28 12:13:28 +0300

    Fetch data from Context and find the specific user

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import colors from '../constants/colors';
+import { FollowersContext } from '../contexts/FollowersContext';
 
-const Profile = ({ navigation }) => (
-  <SafeAreaView>
-    <Text>Follower Profile</Text>
-    <Button
-      title="Go back"
-      color={colors.green}
-      onPress={() => navigation.goBack()}
-    />
-  </SafeAreaView>
-);
+const Profile = ({ navigation }) => {
+  const [user, setUser] = useState({});
+  const { userId, followers } = useContext(FollowersContext);
+
+  useEffect(() => {
+    const found = followers.find((follower) => follower.id === userId);
+    setUser(found);
+  }, [followers, userId]);
+
+  return (
+    <SafeAreaView>
+      <Text>Follower Profile</Text>
+      <Text>{user.login}</Text>
+      <Button
+        title="Go back"
+        color={colors.green}
+        onPress={() => navigation.goBack()}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default Profile;
