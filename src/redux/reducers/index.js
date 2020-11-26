89f9b29e0615f67cdb3a0b202d8f61dbe3507333
Khwilo Kabaka <khwilowatai@gmail.com>
2020-11-26 11:58:32 +0300

    Combine the appUser and user reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 
+import appUser from './appUserReducer';
 import followers from './followerReducer';
+import user from './userReducer';
 
 const rootReducer = combineReducers({
+  appUser,
   followers,
+  user,
 });
 
 export default rootReducer;
