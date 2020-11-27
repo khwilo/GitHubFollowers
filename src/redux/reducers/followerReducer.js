42896637fe45fa94966e9a20acbb022f36de5261
Khwilo Kabaka <khwilowatai@gmail.com>
2020-11-27 13:11:43 +0300

    Add case for updating state on update followers action call

--- src/redux/reducers/followerReducer.js
+++ src/redux/reducers/followerReducer.js
@@ -2,10 +2,14 @@ import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
 const followerReducer = (state = initialState.followers, action = {}) => {
-  if (action.type === types.LOAD_FOLLOWERS_SUCCESS) {
-    return action.payload;
+  switch (action.type) {
+    case types.LOAD_FOLLOWERS_SUCCESS:
+      return action.payload;
+    case types.UPDATE_FOLLOWERS_SUCCESS:
+      return [].concat(state, action.payload);
+    default:
+      return state;
   }
-  return state;
 };
 
 export default followerReducer;
