7dd7796d866adc943704d63a79d7b2258b723e90
Khwilo Kabaka <khwilowatai@gmail.com>
2020-11-27 14:43:31 +0300

    Remove data directory with its contents

--- src/data/index.js
+++
@@ -1,9 +0,0 @@
-// URL: https://docs.github.com/en/free-pro-team@latest/rest/reference/users#list-followers-of-a-user
-
-// ENDPOINTS FOR FETCHING USERS
-
-//  https://api.github.com/users/{username}/followers
-
-// https://api.github.com/users/{username}
-
-// https://api.github.com/users/{username}/followers?page=1
