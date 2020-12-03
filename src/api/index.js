2af7e195fb02742f0765b552de615d65a87c3fee
Khwilo Kabaka <khwilowatai@gmail.com>
2020-12-03 12:57:39 +0300

    Implement logic to fetch the last page of list

--- src/api/index.js
+++ src/api/index.js
@@ -19,3 +19,25 @@ export const fetchUser = async (username) => {
     return error;
   }
 };
+
+export const getLastPage = async (loginName) => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${loginName}/followers`,
+    );
+    const { headers } = response;
+    const endOfList = headers.get('link')
+      ? headers
+          .get('link')
+          .split(',')[1]
+          .split(';')[0]
+          .split('?')[1]
+          .substring(5)
+          .slice(0, -1)
+      : 1;
+    return endOfList;
+  } catch (error) {
+    console.log('ERROR FETCHING LAST PAGE: ', error);
+    throw error;
+  }
+};
