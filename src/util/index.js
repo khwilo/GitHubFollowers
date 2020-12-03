e266c03ff45f62c30cb5d599e70fc3573f72645f
Khwilo Kabaka <khwilowatai@gmail.com>
2020-12-03 12:58:54 +0300

    Export the utility files

---
+++ src/util/index.js
@@ -0,0 +1,5 @@
+import followersUtil from './followers';
+import formatGridData from './formatGridData';
+import truncateText from './truncateText';
+
+export { followersUtil, formatGridData, truncateText };
