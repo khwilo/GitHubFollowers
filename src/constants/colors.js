97baac7a608c42663cd6e9a019f8e6992955c1f5
Khwilo Kabaka <khwilowatai@gmail.com>
2020-12-03 14:47:31 +0300

    Update light gray value

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,7 +3,7 @@ export default {
   black: '#000000',
   green: '#66bb6a',
   gray: '#707070',
-  lightGray: '#cfcfcf',
+  lightGray: '#e0e0e0',
   darkGray: '#707070',
   blueGrey: '#eceff1',
   transparent: 'transparent',
