cbcc43b78d35be052893e2e7696cede5598280ab
Khwilo Kabaka <khwilowatai@gmail.com>
2020-12-03 13:48:34 +0300

    Add light and dark gray colors

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,6 +3,8 @@ export default {
   black: '#000000',
   green: '#66bb6a',
   gray: '#707070',
+  lightGray: '#cfcfcf',
+  darkGray: '#707070',
   blueGrey: '#eceff1',
   transparent: 'transparent',
   lightPurple: '#ae52d4',
