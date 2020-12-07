c92eca6b317757840d384a0dfe33aadbdf15ddf7
Khwilo Kabaka <khwilowatai@gmail.com>
2020-12-07 17:11:02 +0300

    Display the favorited followers

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,12 +1,64 @@
-// TODO: Add followers to favorites
 import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-const Favorites = () => (
-  <SafeAreaView>
-    <Text>Favorites Screen</Text>
-  </SafeAreaView>
-);
+import resetFavorites from '../redux/actions/resetAction';
 
-export default Favorites;
+const FavoriteView = ({ favorite }) => {
+  return (
+    <View style={styles.favoriteViewContainer}>
+      <Image
+        source={{ uri: `${favorite.avatar_url}` }}
+        style={styles.followerImage}
+      />
+      <Text style={styles.followerLogin}>{favorite.login}</Text>
+    </View>
+  );
+};
+
+const Favorites = ({ actions, favorites }) => {
+  return (
+    <View>
+      <Button onPress={() => actions.reset()} title="reset" />
+      {favorites.map((favorite) => (
+        <FavoriteView key={favorite.id} favorite={favorite} />
+      ))}
+    </View>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    favorites: state.favorites,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: {
+      reset: bindActionCreators(resetFavorites, dispatch),
+    },
+  };
+};
+
+const styles = StyleSheet.create({
+  favoriteViewContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  followerImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+  },
+  followerLogin: {
+    fontSize: 24,
+    fontWeight: '700',
+    marginLeft: 10,
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
